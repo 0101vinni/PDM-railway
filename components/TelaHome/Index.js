ba73d74643c8e043f1c5f75c41deded85ea3744d
vinni <vinniciuspgs@hotmail.com>
2023-06-10 14:33:21 -0300

    atualização1

--- components/TelaHome/Index.js
+++ components/TelaHome/Index.js
@@ -1,37 +1,100 @@
 import React from 'react';
-import { StyleSheet,View, Text, Button,Image } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView } from 'react-native';
+import { Button } from '@rneui/themed';
+
 
 const TelaHome = ({ navigation }) => {
+  
+
+  const cards = [
+    {
+      id: 1,
+      image: 'https://a.espncdn.com/combiner/i?img=/photo/2023/0604/r1182185_2_1296x729_16-9.jpg',
+      text: 'Jogo 1 das finais entre conferência, compostos pelos times Denver Nuggets e Miami Heat',
+    },
+    {
+      id: 2,
+      image: 'https://media.npr.org/assets/img/2023/04/01/ap23089090925050_custom-019741384a40a07d3c360b670a8e273e38fe3ce8.jpg',
+      text: 'Jogo realizado na temporada 22/23. times: Utah Jazz, de branco, e San Antonio Spurs, de verde',
+    },
+    {
+      id: 3,
+      image: 'https://cdn.nba.com/manage/2023/05/wemby-smiles-mets-92-scaled.jpg',
+      text: 'Provável primeira pick do draft 2023, Victor Wembanyama',
+    },
+    // ...adicionar mais cards conforme necessário
+  ];
+
   return (
-    <View>
-    <Button
-        title="Ir para a tela da NBA"
-        onPress={() => navigation.navigate('TelaNBA')}
-      />
-      <Button
-        title="Ir para a tela do Futebol Brasileiro"
-        onPress={() => navigation.navigate('TelaFutebolBrasileiro')}
-      />
-      <Text>Aqui voce vera as noticias esportivas do Brasil e do mundo em primeira mao! Basquete, Futebol e tudo sobre o mercado de transferencias voce ve aqui!</Text>
-        <View>
-        <Image source= {{uri: 'https://static.significados.com.br/foto/tipos-de-esporte-og.jpg'}} style={styles.imagem} />
-        </View>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>TelaHome</Text>
+      </View>
+      <ScrollView contentContainerStyle={styles.content}>
+        <Text style={styles.text}>
+          Aqui você verá as notícias esportivas do Brasil e do mundo em primeira mão! Basquete, Futebol e tudo sobre o mercado de transferências você vê aqui!
+        </Text>
+        {cards.map((card) => (
+          <View style={styles.card} key={card.id}>
+            <Image source={{ uri: card.image }} style={styles.cardImage} />
+            <Text style={styles.cardText}>{card.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
+      <View style={styles.footer}>
+        <Button
+          title="Ir para a tela da NBA"
+          onPress={() => navigation.navigate('TelaNBA')}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    imagem: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 400,
-        height: 200,
-    },
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  themeButton: {
+    padding: 5,
+    borderRadius: 20,
+  },
+  content: {
+    flexGrow: 1,
+  },
+  text: {
+    marginBottom: 20,
+  },
+  card: {
+    marginBottom: 10,
+  },
+  cardImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+    borderRadius: 10,
+  },
+  cardText: {
+    fontSize: 16,
+    marginTop: 10,
+  },
+  footer: {
+    alignSelf: 'flex-end',
+    marginTop: 'auto',
+    marginBottom: 10,
+  },
 });
 
 export default TelaHome;
-
-
-
-
-
--- components/TelaNBA/Index.js
+++ components/TelaNBA/Index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Text,FlatList,StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { Icon } from '@rneui/themed';
 import { useDados } from '../../API/Dados';
 
-const TelaNBA = () => {
-
+const TelaNBA = ({ navigation }) => {
   const { dados, isLoading, isError } = useDados();
 
   if (isLoading) {
@@ -14,34 +14,108 @@ const TelaNBA = () => {
     return <Text>Ocorreu um erro ao carregar os dados</Text>;
   }
 
+  const handleTimePress = (item) => {
+    // Lógica para navegar para a próxima página com os detalhes do time
+  };
+
   return (
-    <View>
-      <Text>Tela da NBA</Text>
-      <Button
-        title="Ir para a tela inicial"
-        onPress={() => navigation.navigate('TelaHome')}
-      />
-      <Button
-        title="Ir para a tela do Futebol Brasileiro"
-        onPress={() => navigation.navigate('TelaFutebolBrasileiro')}
-      />
-      <View>
-        <FlatList>
-          data={dados}
-          keyExtractor={(item) => item.objectId}
-          numColums={4}
-          renderItem={({item}) => {
-          <View>
-            <Image source={{ uri: item.logo_time.url }} />
-            <Text>{item.nome}</Text>
-            <Text>{item.cidade}</Text>
-            <Text>{item.estrela_do_time}</Text>
-            </View>
-          }}
-        </FlatList>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>TelaNBA</Text>
+        <ScrollView contentContainerStyle={styles.cardContainer}>
+          {dados.map((item) => (
+            <TouchableOpacity
+              key={item.objectId}
+              style={styles.card}
+              onPress={() => handleTimePress(item)}
+            >
+              <Image source={{ uri: item.logo_time.url }} style={styles.image} />
+              <Text style={styles.timeNome}>{item.nome}</Text>
+              <Text style={styles.timeCidade}>{item.cidade}</Text>
+              <Text style={styles.timeEstrela}>{item.estrela_do_time}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+        <TouchableOpacity style={styles.homeButton} onPress={() => navigation.navigate('TelaHome')}>
+          <Icon name="home" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.arrowButton} onPress={() => navigation.navigate('TelaFutebolBrasileiro')}>
+          <Icon name="arrow-right" size={24} color="white" />
+        </TouchableOpacity>
       </View>
-    </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 20,
+    textAlign: 'center',
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  card: {
+    width: '48%',
+    marginBottom: 20,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
+    padding: 10,
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    marginBottom: 10,
+    resizeMode: 'cover',
+    borderRadius: 4,
+  },
+  timeNome: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  timeCidade: {
+    fontSize: 14,
+    marginBottom: 5,
+  },
+  timeEstrela: {
+    fontSize: 14,
+  },
+  homeButton: {
+    position: 'absolute',
+    top: 30,
+    left: 20,
+    backgroundColor: 'rgba(90, 154, 230, 1)',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  arrowButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: 'rgba(90, 154, 230, 1)',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default TelaNBA;
